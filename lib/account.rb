cf1c2e152c13089df4f22285e96b70f999e4893d
carlostrapet <trapet_llamas@hotmail.com>
2018-03-26 14:33:33 +0100

    :sparkles: added attributes @balance and @transaction_history. all tests passing

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,11 @@
 
 class Account
-  
-  def balance
-    150
+
+  attr_accessor :balance, :transaction_history
+
+  def initialize
+    @balance = 0
+    @transaction_history = []    
   end
+  
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,8 +1,16 @@
 
-describe 'test setup' do
+describe Account do
   subject(:account) { Account.new }
 
-  it 'setup working correctly' do 
-    expect(subject.balance).to eq 150
+  describe '#balance' do
+    it 'defaults to 0' do
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  describe '#transaction_history' do
+    it 'initializes as an empty array' do
+      expect(subject.transaction_history).to eq []
+    end 
   end
 end
